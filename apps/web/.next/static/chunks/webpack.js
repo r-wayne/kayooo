c412b90e4768637b89ee5db7bca61198bc10a56e
wayne rooney <rooneyloowayne@gmail.com>
2025-08-13 08:22:30 +0300

    added admin router

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c47172d4aaa364eb"; }
+/******/ 		__webpack_require__.h = function() { return "82a9675ec4957a57"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
